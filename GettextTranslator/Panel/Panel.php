973739573875d4981e5ddc0b6c2e7a803276f4e0
Adam Schubert <feartohell@seznam.cz>
2014-05-14 14:11:29 +0200

    Format code

--- GettextTranslator/Panel/Panel.php
+++ GettextTranslator/Panel/Panel.php
@@ -6,198 +6,204 @@ use Nette;
 
 class Panel extends Nette\Object implements Nette\Diagnostics\IBarPanel
 {
-	/** @var string */
-	private $xhrHeader = 'X-Translation-Client';
 
-	/** @var string */
-	private $languageKey = 'X-GettextTranslator-Lang';
+  /** @var string */
+  private $xhrHeader = 'X-Translation-Client';
 
-	/** @var string */
-	private $fileKey = 'X-GettextTranslator-File';
+  /** @var string */
+  private $languageKey = 'X-GettextTranslator-Lang';
 
-	/** @var string */
-	private $layout;
+  /** @var string */
+  private $fileKey = 'X-GettextTranslator-File';
 
-	/** @var int */
-	private $height;
+  /** @var string */
+  private $layout;
 
-	/** @var Nette\Application\Application */
-	private $application;
+  /** @var int */
+  private $height;
 
-	/** @var GettextTranslator\Gettext */
-	private $translator;
+  /** @var Nette\Application\Application */
+  private $application;
 
-	/** @var Nette\Http\SessionSection */
-	private $sessionStorage;
+  /** @var GettextTranslator\Gettext */
+  private $translator;
 
-	/** @var Nette\Http\Request */
-	private $httpRequest;
+  /** @var Nette\Http\SessionSection */
+  private $sessionStorage;
 
+  /** @var Nette\Http\Request */
+  private $httpRequest;
 
-	/**
-	 * @param Nette\Application\Application
-	 * @param Gettext\Translator\Gettext 
-	 * @param Nette\Http\Session
-	 * @param Nette\Http\Request
-	 * @param string
-	 * @param int
-	 */
-	public function __construct(Nette\Application\Application $application, Gettext $translator, Nette\Http\Session $session, Nette\Http\Request $httpRequest, $layout, $height)
-	{
-		$this->application = $application;
-		$this->translator = $translator;
-		$this->sessionStorage = $session->getSection(Gettext::$namespace);
-		$this->httpRequest = $httpRequest;
-		$this->height = $height;
-		$this->layout = $layout;
+  /**
+   * @param Nette\Application\Application
+   * @param Gettext\Translator\Gettext 
+   * @param Nette\Http\Session
+   * @param Nette\Http\Request
+   * @param string
+   * @param int
+   */
+  public function __construct(Nette\Application\Application $application, Gettext $translator, Nette\Http\Session $session, Nette\Http\Request $httpRequest, $layout, $height)
+  {
+    $this->application = $application;
+    $this->translator = $translator;
+    $this->sessionStorage = $session->getSection(Gettext::$namespace);
+    $this->httpRequest = $httpRequest;
+    $this->height = $height;
+    $this->layout = $layout;
 
-		$this->processRequest();
-	}
+    $this->processRequest();
+  }
 
+  /**
+   * Return's panel ID
+   * @return string
+   */
+  public function getId()
+  {
+    return __CLASS__;
+  }
 
-	/**
-	 * Return's panel ID
-	 * @return string
-	 */
-	public function getId()
-	{	
-		return __CLASS__;
-	}
+  /**
+   * Returns the code for the panel tab
+   * @return string
+   */
+  public function getTab()
+  {
+    ob_start();
+    require __DIR__ . '/tab.latte';
+    return ob_get_clean();
+  }
 
+  /**
+   * Returns the code for the panel itself
+   * @return string
+   */
+  public function getPanel()
+  {
+    $files = array_keys($this->translator->getFiles());
+    $activeFile = $this->getActiveFile($files);
 
-	/**
-	 * Returns the code for the panel tab
-	 * @return string
-	 */
-	public function getTab()
-	{
-		ob_start();
-		require __DIR__ . '/tab.latte';
-		return ob_get_clean();
-	}
+    $strings = $this->translator->getStrings();
+    $untranslatedStack = isset($this->sessionStorage['stack']) ? $this->sessionStorage['stack'] : array();
+    foreach ($strings as $string => $data)
+    {
+      if (!$data)
+      {
+        $untranslatedStack[$string] = FALSE;
+      }
+    }
+    $this->sessionStorage['stack'] = $untranslatedStack;
 
+    foreach ($untranslatedStack as $string => $value)
+    {
+      if (!isset($strings[$string]))
+      {
+        $strings[$string] = FALSE;
+      }
+    }
 
-	/**
-	 * Returns the code for the panel itself
-	 * @return string
-	 */
-	public function getPanel()
-	{
-		$files = array_keys($this->translator->getFiles());
-		$activeFile = $this->getActiveFile($files);
+    $translator = $this->translator;
 
-		$strings = $this->translator->getStrings();
-		$untranslatedStack = isset($this->sessionStorage['stack']) ? $this->sessionStorage['stack'] : array();
-		foreach ($strings as $string => $data) {
-			if (!$data) {
-				$untranslatedStack[$string] = FALSE;
-			}
-		}
-		$this->sessionStorage['stack'] = $untranslatedStack;
+    ob_start();
+    require __DIR__ . '/panel.latte';
+    return ob_get_clean();
+  }
 
-		foreach ($untranslatedStack as $string => $value) {
-			if (!isset($strings[$string])) {
-				$strings[$string] = FALSE;
-			}
-		}
-		
-		$translator = $this->translator;
+  /**
+   * Handles an incomuing request and saves the data if necessary.
+   */
+  private function processRequest()
+  {
+    if ($this->httpRequest->isPost() && $this->httpRequest->isAjax() && $this->httpRequest->getHeader($this->xhrHeader))
+    {
+      $data = json_decode(file_get_contents('php://input'));
 
-		ob_start();
-		require __DIR__ . '/panel.latte';
-		return ob_get_clean();
-	}
+      if ($data)
+      {
+        if ($this->sessionStorage)
+        {
+          $stack = isset($this->sessionStorage['stack']) ? $this->sessionStorage['stack'] : array();
+        }
 
+        $this->translator->lang = $data->{$this->languageKey};
+        $file = $data->{$this->fileKey};
+        unset($data->{$this->languageKey}, $data->{$this->fileKey});
 
-	/**
-	 * Handles an incomuing request and saves the data if necessary.
-	 */
-	private function processRequest()
-	{
-		if ($this->httpRequest->isPost() && $this->httpRequest->isAjax() && $this->httpRequest->getHeader($this->xhrHeader)) {
-			$data = json_decode(file_get_contents('php://input'));
+        foreach ($data as $string => $value)
+        {
+          $this->translator->setTranslation($string, $value, $file);
+          if ($this->sessionStorage && isset($stack[$string]))
+          {
+            unset($stack[$string]);
+          }
+        }
+        $this->translator->save($file);
 
-			if ($data) {
-				if ($this->sessionStorage) {
-					$stack = isset($this->sessionStorage['stack']) ? $this->sessionStorage['stack'] : array();
-				}
+        if ($this->sessionStorage)
+        {
+          $this->sessionStorage['stack'] = $stack;
+        }
+      }
 
-				$this->translator->lang = $data->{$this->languageKey};
-				$file = $data->{$this->fileKey};
-				unset($data->{$this->languageKey}, $data->{$this->fileKey});
+      exit;
+    }
+  }
 
-				foreach ($data as $string => $value) {
-					$this->translator->setTranslation($string, $value, $file);
-					if ($this->sessionStorage && isset($stack[$string])) {
-						unset($stack[$string]);
-					}
-				}
-				$this->translator->save($file);
+  /**
+   * Return an ordinal number suffix
+   * @param string $count
+   * @return string
+   */
+  protected function ordinalSuffix($count)
+  {
+    switch (substr($count, -1))
+    {
+      case '1':
+        return 'st';
+        break;
+      case '2':
+        return 'nd';
+        break;
+      case '3':
+        return 'rd';
+        break;
+      default:
+        return 'th';
+        break;
+    }
+  }
 
-				if ($this->sessionStorage) {
-					$this->sessionStorage['stack'] = $stack;
-				}
-			}
+  /**
+   * Register this panel
+   * @param Nette\Application\Application
+   * @param GettextTranslator\Gettext
+   * @param Nette\Http\Session
+   * @param Nette\Http\Request
+   * @param int $layout
+   * @param int $height
+   */
+  public static function register(Nette\Application\Application $application, Gettext $translator, Nette\Http\Session $session, Nette\Http\Request $httpRequest, $layout, $height)
+  {
+    Nette\Diagnostics\Debugger::getBar()->addPanel(new static($application, $translator, $session, $httpRequest, $layout, $height));
+  }
 
-			exit;
-		}
-	}
+  /**
+   * Get active file name
+   * @param array
+   * @return string
+   */
+  private function getActiveFile($files)
+  {
+    $tmp = explode(':', $this->application->presenter->name);
 
-
-	/**
-	 * Return an ordinal number suffix
-	 * @param string $count
-	 * @return string
-	 */
-	protected function ordinalSuffix($count)
-	{
-		switch (substr($count, -1)) {
-			case '1':
-				return 'st';
-				break;
-			case '2':
-				return 'nd';
-				break;
-			case '3':
-				return 'rd';
-				break;
-			default:
-				return 'th';
-				break;
-		}
-	}
-
-
-	/**
-	 * Register this panel
-	 * @param Nette\Application\Application
-	 * @param GettextTranslator\Gettext
-	 * @param Nette\Http\Session
-	 * @param Nette\Http\Request
-	 * @param int $layout
-	 * @param int $height
-	 */
-	public static function register(Nette\Application\Application $application, Gettext $translator, Nette\Http\Session $session, Nette\Http\Request $httpRequest, $layout, $height) 
-	{
-		Nette\Diagnostics\Debugger::getBar()->addPanel(new static($application, $translator, $session, $httpRequest, $layout, $height));
-	}
-
-
-	/**
-	 * Get active file name
-	 * @param array
-	 * @return string
-	 */
-	private function getActiveFile($files)
-	{
-		$tmp = explode(':', $this->application->presenter->name);
-
-		if (count($tmp) >= 2 && $module = strtolower($tmp[0])) {
-			return $module;
-
-		} else {
-			return $files[0];
-		}
-	}
+    if (count($tmp) >= 2 && $module = strtolower($tmp[0]))
+    {
+      return $module;
+    }
+    else
+    {
+      return $files[0];
+    }
+  }
 
 }
